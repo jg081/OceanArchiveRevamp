659a24485df263062fdab519bf6e6615f651bba4
William Wood <wcw838@uowmail.edu.au>
2020-06-15 13:11:46 +1000

    basic tabs

---
+++ OceanArchiveRevamp/app.js
@@ -0,0 +1,71 @@
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    }
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+Object.defineProperty(exports, "__esModule", { value: true });
+var React = require('react');
+var ReactDOM = require('react-dom');
+document.body.style.backgroundColor = '#142636';
+document.body.style.fontFamily = 'Roboto';
+document.body.style.color = '#ffffff';
+document.body.style.padding = '0px';
+document.body.style.margin = '0px';
+var tabStyle = {
+    height: '100px',
+    width: '100%',
+    borderCollapse: 'collapse',
+    color: '#000000',
+    padding: '0px',
+    boxSizing: 'border-box',
+};
+var allTabStyle = {
+    padding: '0px',
+    backgroundColor: '#4A74A5',
+    width: '25%',
+};
+var scitechTabStyle = {
+    padding: '0px',
+    backgroundColor: '#0076FF',
+    width: '25%',
+};
+var artTabStyle = {
+    padding: '0px',
+    backgroundColor: '#9013FE',
+    width: '25%',
+};
+var activismTabStyle = {
+    padding: '0px',
+    backgroundColor: '#50E3C2',
+    width: '25%',
+};
+var contentTab = [
+    React.createElement("table", { style: tabStyle },
+        React.createElement("tr", null,
+            React.createElement("th", { style: allTabStyle }, "ALL"),
+            React.createElement("th", { style: scitechTabStyle }, "SCIENCE & TECHNOLOGY"),
+            React.createElement("th", { style: artTabStyle }, "ART"),
+            React.createElement("th", { style: activismTabStyle }, "ACTIVISM")))
+];
+var Homepage = /** @class */ (function (_super) {
+    __extends(Homepage, _super);
+    function Homepage() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Homepage.prototype.render = function () {
+        return (contentTab);
+    };
+    return Homepage;
+}(React.Component));
+exports.Homepage = Homepage;
+ReactDOM.render(React.createElement(Homepage, null), document.getElementById('root'));
+//# sourceMappingURL=app.js.map
---
+++ OceanArchiveRevamp/app.tsx
@@ -0,0 +1,63 @@
+﻿declare var require: any
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+document.body.style.backgroundColor = '#142636';
+document.body.style.fontFamily = 'Roboto';
+document.body.style.color = '#ffffff';
+document.body.style.padding = '0px';
+document.body.style.margin = '0px';
+
+const tabStyle = {
+    height: '100px',
+    width: '100%',
+    borderCollapse: 'collapse',
+    color: '#000000',
+    padding: '0px',
+    boxSizing: 'border-box',
+};
+const allTabStyle = {
+    padding: '0px',
+    backgroundColor: '#4A74A5',
+    width: '25%',
+};
+
+const scitechTabStyle = {
+    padding: '0px',
+    backgroundColor: '#0076FF',
+    width: '25%',
+};
+
+const artTabStyle = {
+    padding: '0px',
+    backgroundColor: '#9013FE',
+    width: '25%',
+};
+
+const activismTabStyle = {
+    padding: '0px',
+    backgroundColor: '#50E3C2',
+    width: '25%',
+};
+
+var contentTab = [
+    <table style={tabStyle}> 
+        <tr>
+            <th style={allTabStyle}>ALL</th>
+            <th style={scitechTabStyle}>SCIENCE & TECHNOLOGY</th>
+            <th style={artTabStyle}>ART</th>
+            <th style={activismTabStyle}>ACTIVISM</th>
+        </tr>
+    </table>
+]
+
+export class Homepage extends React.Component {
+    render() {
+        return (
+            contentTab
+        );
+    }
+}
+
+ReactDOM.render(<Homepage />, document.getElementById('root'));
