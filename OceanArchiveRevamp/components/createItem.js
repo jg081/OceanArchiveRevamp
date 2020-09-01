e5abd3a828e74db4229ba21c0b5a0dbe9132c9ab
William Wood <wcw838@uowmail.edu.au>
2020-09-01 13:27:58 +1000

    Did create item category & tags page layout and functionality to change the main focus area and update other focus checkboxes.

--- OceanArchiveRevamp/components/createItem.js
+++ OceanArchiveRevamp/components/createItem.js
@@ -14,44 +14,131 @@ var __extends = (this && this.__extends) || (function () {
 })();
 Object.defineProperty(exports, "__esModule", { value: true });
 var React = require('react');
+var Constant = require("../constants");
 var reactstrap_1 = require("reactstrap");
+var DetailsPage = /** @class */ (function (_super) {
+    __extends(DetailsPage, _super);
+    function DetailsPage(props) {
+        return _super.call(this, props) || this;
+    }
+    DetailsPage.prototype.render = function () {
+        return (React.createElement("div", { className: 'createItemPage' },
+            React.createElement("div", { className: 'dragAndDrop' }, "DRAG & DROP OR CLICK TO UPLOAD FILE"),
+            React.createElement(reactstrap_1.FormGroup, null,
+                React.createElement(reactstrap_1.Label, { for: 'title' }, "Title"),
+                React.createElement(reactstrap_1.Input, { type: 'text', name: 'title', id: 'title' })),
+            React.createElement(reactstrap_1.FormGroup, null,
+                React.createElement(reactstrap_1.Label, { for: 'desc' }, "Description"),
+                React.createElement(reactstrap_1.Input, { type: 'textarea', name: 'desc', id: 'desc' })),
+            React.createElement(reactstrap_1.FormGroup, null,
+                React.createElement(reactstrap_1.Label, { for: 'creator' }, "Creator(s) / Author(s)"),
+                React.createElement(reactstrap_1.Input, { type: 'text', name: 'creator', id: 'creator' })),
+            React.createElement(reactstrap_1.FormGroup, null,
+                React.createElement(reactstrap_1.Label, { for: 'dateStart' }, "Date Started/Made"),
+                React.createElement(reactstrap_1.Input, { type: 'date', name: 'dateStart', id: 'dateStart' })),
+            React.createElement(reactstrap_1.FormGroup, null,
+                React.createElement(reactstrap_1.Label, { for: 'dateFinish' }, "Date Finished"),
+                React.createElement(reactstrap_1.Input, { type: 'date', name: 'dateFinish', id: 'dateFinish' })),
+            React.createElement(reactstrap_1.FormGroup, null,
+                React.createElement(reactstrap_1.Label, { for: 'url' }, "URL"),
+                React.createElement(reactstrap_1.Input, { type: 'url', name: 'url', id: 'url' })),
+            React.createElement(reactstrap_1.FormGroup, null,
+                React.createElement(reactstrap_1.Label, { for: 'lang' }, "Language"),
+                React.createElement(reactstrap_1.Input, { type: 'text', name: 'lang', id: 'lang' }))));
+    };
+    return DetailsPage;
+}(React.Component));
+var CategoryAndTagsPage = /** @class */ (function (_super) {
+    __extends(CategoryAndTagsPage, _super);
+    function CategoryAndTagsPage(props) {
+        var _this = _super.call(this, props) || this;
+        _this.setMainFocus = function (index) {
+            if (index != _this.state.activeFocus) {
+                _this.props.setMainFocus(index);
+                _this.setState({
+                    activeFocus: index
+                });
+                //console.log(index, " checked ", this.checkBoxes[index].current.checked);
+                _this.checkBoxes[index].current.checked = false;
+            }
+        };
+        _this.state = {
+            activeFocus: 0
+        };
+        _this.checkBoxes = [
+            React.createRef(),
+            React.createRef(),
+            React.createRef()
+        ];
+        return _this;
+    }
+    CategoryAndTagsPage.prototype.render = function () {
+        var _this = this;
+        return (React.createElement("div", { className: 'createItemPage' },
+            "Categories",
+            React.createElement("hr", null),
+            React.createElement(reactstrap_1.FormGroup, null,
+                React.createElement(reactstrap_1.Label, { for: 'focusMain' }, "Main Focus Area"),
+                React.createElement("input", { type: 'hidden', name: 'focusMain', value: this.mainFocus }),
+                React.createElement(reactstrap_1.ButtonGroup, { className: 'mainFocus' },
+                    React.createElement(reactstrap_1.Button, { className: 'left', type: 'button', style: {
+                            backgroundColor: this.state.activeFocus == 0 ? Constant.SECONDARY_COLOUR : Constant.MAIN_COLOUR
+                        }, onClick: function () { return _this.setMainFocus(0); } }, "Science & Tech"),
+                    React.createElement(reactstrap_1.Button, { type: 'button', style: { backgroundColor: this.state.activeFocus == 1 ? Constant.SECONDARY_COLOUR : Constant.MAIN_COLOUR }, onClick: function () { return _this.setMainFocus(1); } }, "Art"),
+                    React.createElement(reactstrap_1.Button, { className: 'right', type: 'button', style: { backgroundColor: this.state.activeFocus == 2 ? Constant.SECONDARY_COLOUR : Constant.MAIN_COLOUR }, onClick: function () { return _this.setMainFocus(2); } }, "Activism"))),
+            React.createElement(reactstrap_1.FormGroup, { check: true },
+                React.createElement(reactstrap_1.Label, null, "Other Focus Areas (optional)"),
+                React.createElement(reactstrap_1.Label, { check: true, style: { display: this.state.activeFocus == 0 ? 'none' : 'inline-block' } },
+                    React.createElement("input", { type: 'checkbox', name: 'other', value: 'sci', style: { display: this.state.activeFocus == 0 ? 'none' : 'inline-block' }, ref: this.checkBoxes[0] }),
+                    ' ',
+                    "Science & Tech"),
+                React.createElement(reactstrap_1.Label, { check: true, style: { display: this.state.activeFocus == 1 ? 'none' : 'inline-block' } },
+                    React.createElement("input", { type: 'checkbox', name: 'other', value: 'art', style: { display: this.state.activeFocus == 1 ? 'none' : 'inline-block' }, ref: this.checkBoxes[1] }),
+                    ' ',
+                    "Art"),
+                React.createElement(reactstrap_1.Label, { check: true, style: { display: this.state.activeFocus == 2 ? 'none' : 'inline-block' } },
+                    React.createElement("input", { type: 'checkbox', name: 'other', value: 'act', style: { display: this.state.activeFocus == 2 ? 'none' : 'inline-block' }, ref: this.checkBoxes[2] }),
+                    ' ',
+                    "Activism")),
+            React.createElement(reactstrap_1.FormGroup, null,
+                React.createElement(reactstrap_1.Label, { for: 'cat' }, "Object Category"),
+                React.createElement(reactstrap_1.Input, { type: 'select', name: 'cat' },
+                    React.createElement("option", null, "Other"),
+                    React.createElement("option", null, "painting"),
+                    React.createElement("option", null, "video"))),
+            React.createElement("div", { style: { height: '24px' } }),
+            "Tags",
+            React.createElement("hr", null),
+            React.createElement(reactstrap_1.FormGroup, null,
+                React.createElement(reactstrap_1.Label, { for: 'concept' }, "Concept"),
+                React.createElement(reactstrap_1.Input, { type: 'select', name: 'concept' })),
+            React.createElement(reactstrap_1.FormGroup, null,
+                React.createElement(reactstrap_1.Label, { for: 'keyword' }, "Keywords (optional)"),
+                React.createElement(reactstrap_1.Input, { type: 'select', name: 'keyword' }))));
+    };
+    return CategoryAndTagsPage;
+}(React.Component));
 var CreateItem = /** @class */ (function (_super) {
     __extends(CreateItem, _super);
     function CreateItem(props) {
         var _this = _super.call(this, props) || this;
+        _this.focusAreas = ['sci', 'art', 'act'];
+        _this.setMainFocus = function (index) {
+            console.log("mainFocus Before: ", _this.mainFocus);
+            _this.mainFocus = _this.focusAreas[index];
+            console.log("mainFocus After: ", _this.mainFocus);
+        };
         _this.formNumbers = [1, 2, 3, 4];
         _this.formPages = _this.formNumbers.map(function (i) {
             switch (i) {
                 //Details
                 case 1:
-                    return (React.createElement(reactstrap_1.CarouselItem, { className: 'createItemPage', key: '1' },
-                        React.createElement("div", { className: 'dragAndDrop' }, "DRAG & DROP OR CLICK TO UPLOAD FILE"),
-                        React.createElement(reactstrap_1.FormGroup, null,
-                            React.createElement(reactstrap_1.Label, { for: 'title' }, "Title"),
-                            React.createElement(reactstrap_1.Input, { type: 'text', name: 'title', id: 'title' })),
-                        React.createElement(reactstrap_1.FormGroup, null,
-                            React.createElement(reactstrap_1.Label, { for: 'desc' }, "Description"),
-                            React.createElement(reactstrap_1.Input, { type: 'textarea', name: 'desc', id: 'desc' })),
-                        React.createElement(reactstrap_1.FormGroup, null,
-                            React.createElement(reactstrap_1.Label, { for: 'creator' }, "Creator(s) / Author(s)"),
-                            React.createElement(reactstrap_1.Input, { type: 'text', name: 'creator', id: 'creator' })),
-                        React.createElement(reactstrap_1.FormGroup, null,
-                            React.createElement(reactstrap_1.Label, { for: 'dateStart' }, "Date Started/Made"),
-                            React.createElement(reactstrap_1.Input, { type: 'date', name: 'dateStart', id: 'dateStart' })),
-                        React.createElement(reactstrap_1.FormGroup, null,
-                            React.createElement(reactstrap_1.Label, { for: 'dateFinish' }, "Date Finished"),
-                            React.createElement(reactstrap_1.Input, { type: 'date', name: 'dateFinish', id: 'dateFinish' })),
-                        React.createElement(reactstrap_1.FormGroup, null,
-                            React.createElement(reactstrap_1.Label, { for: 'url' }, "URL"),
-                            React.createElement(reactstrap_1.Input, { type: 'url', name: 'url', id: 'url' })),
-                        React.createElement(reactstrap_1.FormGroup, null,
-                            React.createElement(reactstrap_1.Label, { for: 'lang' }, "Language"),
-                            React.createElement(reactstrap_1.Input, { type: 'text', name: 'lang', id: 'lang' }))));
+                    return (React.createElement(reactstrap_1.CarouselItem, { key: '1' },
+                        React.createElement(DetailsPage, null)));
                 //Category & Tags
                 case 2:
-                    return (React.createElement(reactstrap_1.CarouselItem, { className: 'createItemPage', key: '2' },
-                        "Categories",
-                        React.createElement("hr", null)));
+                    return (React.createElement(reactstrap_1.CarouselItem, { key: '2' },
+                        React.createElement(CategoryAndTagsPage, { setMainFocus: _this.setMainFocus })));
                 //Regions & Legal
                 case 3:
                     return (React.createElement(reactstrap_1.CarouselItem, { className: 'createItemPage', key: '3' },
@@ -81,9 +168,11 @@ var CreateItem = /** @class */ (function (_super) {
             activeIndex: 0,
             animating: false
         };
+        var mainFocus;
         return _this;
     }
     CreateItem.prototype.render = function () {
+        this.mainFocus = this.focusAreas[0];
         return (React.createElement(reactstrap_1.Form, { className: 'creationContainer' },
             React.createElement("div", { className: 'creationHeader' },
                 "Create Item",
--- OceanArchiveRevamp/components/createItem.tsx
+++ OceanArchiveRevamp/components/createItem.tsx
@@ -2,6 +2,7 @@
 
 var React = require('react');
 
+import * as Constant from '../constants';
 import {
     Carousel,
     CarouselItem,
@@ -10,8 +11,157 @@ import {
     Form,
     FormGroup,
     Label,
-    Input
+    Input,
+    ButtonGroup,
+    Button
 } from 'reactstrap';
+
+class DetailsPage extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className='createItemPage'>
+                <div className='dragAndDrop'>
+                    DRAG & DROP OR CLICK TO UPLOAD FILE
+                        </div>
+                <FormGroup>
+                    <Label for='title'>Title</Label>
+                    <Input type='text' name='title' id='title' />
+                </FormGroup>
+                <FormGroup>
+                    <Label for='desc'>Description</Label>
+                    <Input type='textarea' name='desc' id='desc' />
+                </FormGroup>
+                <FormGroup>
+                    <Label for='creator'>Creator(s) / Author(s)</Label>
+                    <Input type='text' name='creator' id='creator' />
+                </FormGroup>
+                <FormGroup>
+                    <Label for='dateStart'>Date Started/Made</Label>
+                    <Input type='date' name='dateStart' id='dateStart' />
+                </FormGroup>
+                <FormGroup>
+                    <Label for='dateFinish'>Date Finished</Label>
+                    <Input type='date' name='dateFinish' id='dateFinish' />
+                </FormGroup>
+                <FormGroup>
+                    <Label for='url'>URL</Label>
+                    <Input type='url' name='url' id='url' />
+                </FormGroup>
+                <FormGroup>
+                    <Label for='lang'>Language</Label>
+                    <Input type='text' name='lang' id='lang' />
+                </FormGroup>
+            </div>
+        );
+    }
+}
+
+class CategoryAndTagsPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            activeFocus: 0
+        };
+        this.checkBoxes = [
+            React.createRef(),
+            React.createRef(),
+            React.createRef()
+        ];
+    }
+
+    setMainFocus = (index) => {
+        if (index != this.state.activeFocus) {
+            this.props.setMainFocus(index);
+            this.setState({
+                activeFocus: index
+            });
+            //console.log(index, " checked ", this.checkBoxes[index].current.checked);
+            this.checkBoxes[index].current.checked = false;
+        }
+    }
+
+    render() {
+        return (
+            <div className='createItemPage'>
+                Categories
+                        <hr />
+                <FormGroup>
+                    <Label for='focusMain'>Main Focus Area</Label>
+                    <input type='hidden' name='focusMain' value={this.mainFocus} />
+                    <ButtonGroup className='mainFocus'>
+                        <Button className='left'
+                            type='button'
+                            style={{
+                                backgroundColor: this.state.activeFocus == 0 ? Constant.SECONDARY_COLOUR : Constant.MAIN_COLOUR }}
+                            onClick={() => this.setMainFocus(0)}
+                        >Science & Tech</Button>
+                        <Button type='button'
+                            style={{ backgroundColor: this.state.activeFocus == 1 ? Constant.SECONDARY_COLOUR : Constant.MAIN_COLOUR }}
+                            onClick={() => this.setMainFocus(1)}
+                        >Art</Button>
+                        <Button className='right'
+                            type='button'
+                            style={{ backgroundColor: this.state.activeFocus == 2 ? Constant.SECONDARY_COLOUR : Constant.MAIN_COLOUR }}
+                            onClick={() => this.setMainFocus(2)}
+                        >Activism</Button>
+                    </ButtonGroup>
+                </FormGroup>
+                <FormGroup check>
+                    <Label>Other Focus Areas (optional)</Label>
+                    <Label check style={{ display: this.state.activeFocus == 0 ? 'none' : 'inline-block' }}>
+                        <input type='checkbox'
+                            name='other'
+                            value='sci'
+                            style={{ display: this.state.activeFocus == 0 ? 'none' : 'inline-block' }}
+                            ref={this.checkBoxes[0]}
+                        />{' '}Science & Tech
+                                </Label>
+                    <Label check style={{ display: this.state.activeFocus == 1 ? 'none' : 'inline-block' }}>
+                        <input type='checkbox'
+                            name='other'
+                            value='art'
+                            style={{ display: this.state.activeFocus == 1 ? 'none' : 'inline-block' }}
+                            ref={this.checkBoxes[1]}
+                        />{' '}Art
+                                </Label>
+                    <Label check style={{ display: this.state.activeFocus == 2 ? 'none' : 'inline-block' }}>
+                        <input type='checkbox'
+                            name='other'
+                            value='act'
+                            style={{ display: this.state.activeFocus == 2 ? 'none' : 'inline-block' }}
+                            ref={this.checkBoxes[2]}
+                        />{' '}Activism
+                                </Label>
+                </FormGroup>
+                <FormGroup>
+                    <Label for='cat'>Object Category</Label>
+                    <Input type='select' name='cat'>
+                        <option>Other</option>
+                        <option>painting</option>
+                        <option>video</option>
+                    </Input>
+                </FormGroup>
+                <div style={{ height: '24px' }} />
+                Tags
+                        <hr />
+                <FormGroup>
+                    <Label for='concept'>Concept</Label>
+                    <Input type='select' name='concept'>
+                    </Input>
+                </FormGroup>
+                <FormGroup>
+                    <Label for='keyword'>Keywords (optional)</Label>
+                    <Input type='select' name='keyword'>
+                    </Input>
+                </FormGroup>
+            </div>
+        );
+    }
+}
 
 export default class CreateItem extends React.Component {
     constructor(props) {
@@ -20,6 +170,15 @@ export default class CreateItem extends React.Component {
             activeIndex: 0,
             animating: false
         };
+        var mainFocus;
+    }
+
+    focusAreas = ['sci', 'art', 'act'];
+
+    setMainFocus = (index) => {
+        //console.log("mainFocus Before: ", this.mainFocus);
+        this.mainFocus = this.focusAreas[index];
+        //console.log("mainFocus After: ", this.mainFocus);
     }
 
     formNumbers = [1, 2, 3, 4];
@@ -29,47 +188,15 @@ export default class CreateItem extends React.Component {
             //Details
             case 1:
                 return (
-                    <CarouselItem className='createItemPage' key='1'>
-                        <div className='dragAndDrop'>
-                            DRAG & DROP OR CLICK TO UPLOAD FILE
-                        </div>
-                        <FormGroup>
-                            <Label for='title'>Title</Label>
-                            <Input type='text' name='title' id='title' />
-                        </FormGroup>
-                        <FormGroup>
-                            <Label for='desc'>Description</Label>
-                            <Input type='textarea' name='desc' id='desc' />
-                        </FormGroup>
-                        <FormGroup>
-                            <Label for='creator'>Creator(s) / Author(s)</Label>
-                            <Input type='text' name='creator' id='creator' />
-                        </FormGroup>
-                        <FormGroup>
-                            <Label for='dateStart'>Date Started/Made</Label>
-                            <Input type='date' name='dateStart' id='dateStart' />
-                        </FormGroup>
-                        <FormGroup>
-                            <Label for='dateFinish'>Date Finished</Label>
-                            <Input type='date' name='dateFinish' id='dateFinish' />
-                        </FormGroup>
-                        <FormGroup>
-                            <Label for='url'>URL</Label>
-                            <Input type='url' name='url' id='url' />
-                        </FormGroup>
-                        <FormGroup>
-                            <Label for='lang'>Language</Label>
-                            <Input type='text' name='lang' id='lang' />
-                        </FormGroup>
+                    <CarouselItem key='1'>
+                        <DetailsPage />
                     </CarouselItem>
                 );
             //Category & Tags
             case 2:
                 return (
-                    <CarouselItem className='createItemPage' key='2'>
-                        Categories
-                        <hr />
-
+                    <CarouselItem key='2'>
+                        <CategoryAndTagsPage setMainFocus={this.setMainFocus} />
                     </CarouselItem>
                 );
             //Regions & Legal
@@ -110,6 +237,7 @@ export default class CreateItem extends React.Component {
     }
 
     render() {
+        this.mainFocus = this.focusAreas[0];
         return (
             <Form className='creationContainer'>
                 <div className='creationHeader'>
